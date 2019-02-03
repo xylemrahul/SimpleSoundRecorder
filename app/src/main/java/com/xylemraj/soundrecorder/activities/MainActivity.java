f0961dc3bb5959ca5d150b0124a5b0a7e05993a9
xylemraj <rajspeed.20@gmail.com>
2019-02-03 22:41:05 +0530

    first commit

---
+++ app/src/main/java/com/xylemraj/soundrecorder/activities/MainActivity.java
@@ -0,0 +1,31 @@
+package com.xylemraj.soundrecorder.activities;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.danielkim.soundrecorder.R;
+import com.xylemraj.soundrecorder.fragments.RecordFragment;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.container, RecordFragment.newInstance(), "");
+        transaction.commit();
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setPopupTheme(R.style.ThemeOverlay_AppCompat_Light);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+    }
+}
---
+++ app/src/main/java/com/xylemraj/soundrecorder/fragments/RecordFragment.java
@@ -0,0 +1,139 @@
+package com.xylemraj.soundrecorder.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.SystemClock;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.danielkim.soundrecorder.R;
+import com.xylemraj.soundrecorder.RecordingService;
+import com.melnykov.fab.FloatingActionButton;
+
+import java.io.File;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * to handle interaction events.
+ * Use the {@link RecordFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class RecordFragment extends Fragment {
+
+    //Recording controls
+    private FloatingActionButton mRecordButton = null;
+    private Button mPauseButton = null;
+
+    private TextView mRecordingPrompt;
+    private int mRecordPromptCount = 0;
+
+    private boolean mStartRecording = true;
+    private boolean mPauseRecording = true;
+
+    private Chronometer mChronometer = null;
+    long timeWhenPaused = 0; //stores time when user clicks pause button
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment Record_Fragment.
+     */
+    public static RecordFragment newInstance() {
+        RecordFragment f = new RecordFragment();
+        return f;
+    }
+
+    public RecordFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View recordView = inflater.inflate(R.layout.fragment_record, container, false);
+
+        mChronometer = (Chronometer) recordView.findViewById(R.id.chronometer);
+        //update recording prompt text
+        mRecordingPrompt = (TextView) recordView.findViewById(R.id.recording_status_text);
+
+        mRecordButton = (FloatingActionButton) recordView.findViewById(R.id.btnRecord);
+        mRecordButton.setColorNormal(getResources().getColor(R.color.primary));
+        mRecordButton.setColorPressed(getResources().getColor(R.color.primary_dark));
+        mRecordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onRecord(mStartRecording);
+                mStartRecording = !mStartRecording;
+            }
+        });
+
+        return recordView;
+    }
+
+    // Recording Start/Stop
+    //TODO: recording pause
+    private void onRecord(boolean start){
+
+        Intent intent = new Intent(getActivity(), RecordingService.class);
+
+        if (start) {
+            // start recording
+
+            Toast.makeText(getActivity(),R.string.toast_recording_start,Toast.LENGTH_SHORT).show();
+            File folder = new File(Environment.getExternalStorageDirectory() + "/SoundRecorder");
+            if (!folder.exists()) {
+                //folder /SoundRecorder doesn't exist, create the folder
+                folder.mkdir();
+            }
+
+            //start Chronometer
+            mChronometer.setBase(SystemClock.elapsedRealtime());
+            mChronometer.start();
+            mChronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+                @Override
+                public void onChronometerTick(Chronometer chronometer) {
+                    if (mRecordPromptCount == 0) {
+                        mRecordingPrompt.setText(getString(R.string.record_in_progress) + ".");
+                    } else if (mRecordPromptCount == 1) {
+                        mRecordingPrompt.setText(getString(R.string.record_in_progress) + "..");
+                    } else if (mRecordPromptCount == 2) {
+                        mRecordingPrompt.setText(getString(R.string.record_in_progress) + "...");
+                        mRecordPromptCount = -1;
+                    }
+
+                    mRecordPromptCount++;
+                }
+            });
+
+            //start RecordingService
+            getActivity().startService(intent);
+            //keep screen on while recording
+            getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+            mRecordingPrompt.setText(getString(R.string.record_in_progress) + ".");
+            mRecordPromptCount++;
+
+        } else {
+            //stop recording
+            mRecordButton.setImageResource(R.drawable.ic_mic_white_36dp);
+            //mPauseButton.setVisibility(View.GONE);
+            mChronometer.stop();
+            mChronometer.setBase(SystemClock.elapsedRealtime());
+            timeWhenPaused = 0;
+            mRecordingPrompt.setText(getString(R.string.record_prompt));
+
+            getActivity().stopService(intent);
+            //allow the screen to turn off again once recording is finished
+            getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
+    }
+}
